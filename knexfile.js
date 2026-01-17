cac49c68caf927b6a86daed20318d14c7c1076b3
ex-commando <francismission61@yahoo.com>
2026-01-17 12:29:03 +0100

    Update docs, add deployment guide and integration tests

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 module.exports = {
     development: {
         client: "mysql2",
-        connection: {
+        connection: process.env.DATABASE_URL || {
             host: process.env.DB_HOST || "127.0.0.1",
             port: Number(process.env.DB_PORT) || 3306,
             user: process.env.DB_USER || "root",
